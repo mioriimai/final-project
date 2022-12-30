57b6aae755a7842cae53b492d7c4138e70ca687e
Miori Imai <imamio1227@gmail.com>
2022-12-30 20:14:02 +0000

    Import tables from database designer into schema.sql. #1

--- database/schema.sql
+++ database/schema.sql
@@ -5,3 +5,27 @@ set client_min_messages to warning;
 drop schema "public" cascade;
 
 create schema "public";
+
+create table "public"."users" (
+  "userId"         serial,
+  "username"       text           not null,
+  "email"          text           not null,
+  "hashedPassword" text           not null,
+  "createdAt"      timestamptz(6) not null default now(),
+  primary key ("userId"),
+  unique ("username")
+);
+
+create table "public"."items" (
+  "itemId"              serial,
+  "originalImageUrl"    text           not null,
+  "bgRemovedImageUrl"   text           not null,
+  "category"            text           not null,
+  "brand"               text           not null,
+  "color"               text           not null,
+  "note"                text           not null,
+  "userId"              serial         not null,
+  "isFavorite"          BOOLEAN,
+  "createdAt"      timestamptz(6) not null default now(),
+  primary key ("itemId")
+);
