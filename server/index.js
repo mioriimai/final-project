1dfa937b01dbb5a5c326dbcd0d1c860f4e54526b
Miori Imai <imamio1227@gmail.com>
2022-12-30 23:41:34 +0000

    Update server/index.js to ... send a POST request to create new item into the items table. #1

--- server/index.js
+++ server/index.js
@@ -2,13 +2,53 @@ require('dotenv/config');
 const express = require('express');
 const staticMiddleware = require('./static-middleware');
 const errorMiddleware = require('./error-middleware');
+const ClientError = require('./client-error');
+const pg = require('pg');
+
+// create a database connection object to use the pg package.
+const db = new pg.Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 
 const app = express();
 
 app.use(staticMiddleware);
+app.use(express.json());
 
-app.get('/api/hello', (req, res) => {
-  res.json({ hello: 'world' });
+app.post('/api/form-item', (req, res, next) => {
+  // varidate the "inputs" first.
+  const newItem = req.body;
+  if ('originalImageUrl' in newItem === false || 'bgRemovedImageUrl' in newItem === false || 'category' in newItem === false || 'brand' in newItem === false || 'color' in newItem === false || 'isFavorite' in newItem === false || 'userId' in newItem === false || !Number.isInteger(newItem.userId) || newItem.userId <= 0) {
+    throw new ClientError(400, 'An invalid/missing information.');
+  }
+  // query the database
+  const sql = `
+    insert into "items" ("originalImageUrl", "bgRemovedImageUrl", "category", "brand", "color", "isFavorite", "userId")
+    values ($1, $2, $3, $4, $5, $6, $7)
+    returning *
+  `;
+  // send the user input in a separate array instead of putting the user input directory into our query
+  const params = [newItem.originalImageUrl, newItem.bgRemovedImageUrl, newItem.category, newItem.brand, newItem.color, newItem.isFavorite, newItem.userId];
+
+  db.query(sql, params)
+    .then(result => {
+      // the query succeeded
+      // respond to the client with the status code 200 and created grade object
+      res.status(201).json(result.rows[0]);
+    })
+    .catch(err => {
+      // the query failed for some reason
+      // possibly due to a syntax error in the SQL statement
+      // print the error to STDERR (the terminal) for debugging purposes
+      console.error(err);
+      // respond to the client with a generic 500 error message
+      res.status(500).json({
+        error: 'An unexpected error occurred.'
+      });
+    });
 });
 
 app.use(errorMiddleware);
