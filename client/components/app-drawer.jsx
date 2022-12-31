67d2f80ca7097f2adf2fb2248e8ceb1345b5f90a
Miori Imai <imamio1227@gmail.com>
2022-12-31 02:19:36 +0000

    Add a class AppDrawer in the file./Add constructor, super, state. #1

--- client/components/app-drawer.jsx
+++ client/components/app-drawer.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
 
 export default class AppDrawer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isMenuOpened: false
+    };
+    this.handleMenuIconClick = this.handleMenuIconClick.bind(this);
+    this.handleLinksOrShadeClick = this.handleLinksOrShadeClick.bind(this);
+  }
 
   render() {
     return (
