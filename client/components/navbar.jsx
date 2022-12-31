a92903ea54d582d48678a6a6f34e65f6482b594c
Miori Imai <imamio1227@gmail.com>
2022-12-31 00:05:36 +0000

    Create new component named navber.jsx./Import React and AppContext./Add a class Navber in navber.jsx. #1

--- client/components/navbar.jsx
+++ client/components/navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default class HelloWorld extends React.Component {
+export default class Navbar extends React.Component {
   render() {
     return (
       <h1>Hello, World!</h1>
--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import HelloWorld from '../components/hello-world';
 
-export default function Home(props) {
-  return (
-    <div>
-      <HelloWorld />
-    </div>
-  );
+export default class Home extends React.Component {
+  render() {
+    return (
+      <div>
+        <HelloWorld />
+      </div>
+    );
+  }
 }
