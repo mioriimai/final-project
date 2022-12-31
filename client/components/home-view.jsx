bef2b5a04a7bde16bcec1251ed1d7ad746a558c8
Miori Imai <imamio1227@gmail.com>
2022-12-31 03:49:45 +0000

    Fill the render function in home-view.jsx. #1

--- client/components/home-view.jsx
+++ client/components/home-view.jsx
@@ -3,16 +3,16 @@ import React from 'react';
 export default class HomeView extends React.Component {
   render() {
     return (
-      <>
-        <p>Wanna organize your closet digitally?</p>
+      <div className='home-view-wrapper'>
+        <p className='wanna-organize'>Wanna organize your closet digitally?</p>
         <a className='add-item-button' href="#add-item">Add an Item</a>
         <br />
         <a className='add-outfit-button' href="#add-outfit">Add an Outfit</a>
-        <p>What are you going to wear today?</p>
+        <p className='what-to-wear'>What are you going to wear today?</p>
         <a className='see-items-button' href="#items">See Items</a>
         <br />
         <a className='see-outfits-button' href="#outfits">See Outfits</a>
-      </>
+      </div>
     );
   }
 }
