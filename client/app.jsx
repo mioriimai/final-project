d6df41e16eca634b883d4a70b16b31e4c54d9370
Miori Imai <imamio1227@gmail.com>
2022-12-31 04:10:58 +0000

    Add constructor in App component. #1

--- client/app.jsx
+++ client/app.jsx
@@ -2,8 +2,16 @@ import React from 'react';
 import Home from './pages/home';
 // import AppContext from './lib/app-context';
 import Navbar from './components/navbar';
+import parseRoute from './lib/parse-route';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      route: parseRoute(window.location.hash)
+    };
+  }
+
   render() {
     return (
     // <AppContext.Provider>
