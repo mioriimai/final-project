4c60175a5dfdc1d3f44c2f7972f6e26439b9dd61
Miori Imai <imamio1227@gmail.com>
2022-12-31 00:41:05 +0000

    In the class, fill the render function.#1

--- client/components/navbar.jsx
+++ client/components/navbar.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import AppContext from '../lib/app-context';
 
 export default class Navbar extends React.Component {
   render() {
+    // const { user, handleSignOut } = this.context;
     return (
-      <h1>Hello, World!</h1>
+      <nav className='navbar-wrapper'>
+        <a className='your-closet' href='#home'>Your Closet</a>
+        <i className="fa-solid fa-bars" />
+      </nav>
     );
   }
 }
+Navbar.contextType = AppContext;
