7ed5036a61e505247808622ec8391c4a03932900
Miori Imai <imamio1227@gmail.com>
2022-12-31 01:03:28 +0000

    Check that the navbar looks good on both mobile and desktop screens. #1

--- client/app.jsx
+++ client/app.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import Home from './pages/home';
-import AppContext from './lib/app-context';
+// import AppContext from './lib/app-context';
 import Navbar from './components/navbar';
 
 export default class App extends React.Component {
   render() {
     return (
-      <AppContext.Provider>
-        <>
-          <Navbar />
-          <Home/>
-        </>
-      </AppContext.Provider>
+    // <AppContext.Provider>
+      <>
+        <Navbar />
+        <Home/>
+      </>
+    // </AppContext.Provider>
     );
   }
 }
