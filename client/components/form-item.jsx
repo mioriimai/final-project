91ca1a4337e4e3cc043854597a80c18365fc1942
Miori Imai <imamio1227@gmail.com>
2023-01-03 11:52:10 +0000

    Add handleImageChange function. #1

--- client/components/form-item.jsx
+++ client/components/form-item.jsx
@@ -4,14 +4,28 @@ export default class FormItem extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      category: 'None'
     };
+    this.handleCategoryChange = this.handleCategoryChange.bind(this);
+  }
+
+  handleCategoryChange(event) {
+    this.setState({
+      category: event.target.value
+    });
   }
 
   render() {
     return (
-      <div className='item-view-wrapper'>
+      <div className='form-item-wrapper'>
         <h1>{this.props.title}</h1>
+        <form>
+          <input
+            type="text"
+            name="category"
+            value={this.state.category}
+            onChange={this.handleCategoryChange} />
+        </form>
       </div>
     );
   }
