de4232c9aa0971a1897a16d4999d2512f6e85e76
Miori Imai <imamio1227@gmail.com>
2023-01-08 11:17:56 +0000

    Add handleMouseEnter/hondleMouseLeave functions to show the notes when user hover over the element. #2

--- client/components/items.jsx
+++ client/components/items.jsx
@@ -4,8 +4,11 @@ export default class Items extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      items: []
+      items: [],
+      showNotes: false
     };
+    this.handleMouseEnter = this.handleMouseEnter.bind(this);
+    this.handleMouseLeave = this.handleMouseLeave.bind(this);
   }
 
   componentDidMount() {
@@ -14,11 +17,58 @@ export default class Items extends React.Component {
       .then(items => this.setState({ items }));
   }
 
+  handleMouseEnter(event) {
+    this.setState({
+      showNotes: true
+    });
+  }
+
+  handleMouseLeave() {
+    this.setState({
+      showNotes: false
+    });
+  }
+
   render() {
+    const hoverClassName = this.state.showNotes ? 'shadow-wrapper' : 'shadow-wrapper hidden';
     return (
       <div className='items-container'>
-        <h1>{this.props.content}</h1>
+        <p className='items'>{this.props.content}</p>
+
+        <div className='item-list-wrapper'>
+          {
+            this.state.items.map(item => (
+              <div key={item.itemId} className="item-wrapper">
+                <Item
+                  item={item}
+                  handleMouseEnter={this.handleMouseEnter}
+                  handleMouseLeave={this.handleMouseLeave}
+                  hoverClassName={hoverClassName}/>
+              </div>
+            ))
+          }
+        </div>
       </div>
     );
   }
+}
+
+function Item(props) {
+  const { originalImage, notes, itemId } = props.item;
+
+  return (
+    <a href={`#items?itemId=${itemId}`} className='item-button' >
+      <div className='position'>
+        <img
+        src={originalImage}
+        alt={`Item ${itemId}`}
+        className="item-image"
+        onMouseEnter={props.handleMouseEnter}
+        />
+        <div className={props.hoverClassName} onMouseLeave={props.handleMouseLeave} >
+          <p className='items-notes'>{notes}</p>
+        </div>
+      </div>
+    </a>
+  );
 }
