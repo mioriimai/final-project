b115bfe8b6c1254c29464a47ff2dd82e39e75882
Miori Imai <imamio1227@gmail.com>
2023-02-18 20:08:57 +0000

    Add a class EditItem to the component. #3

---
+++ client/components/edit-item.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+export default class EditItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      originalImage: '',
+      bgRemovedImage: 'None',
+      category: '',
+      brand: '',
+      color: '',
+      notes: '',
+      preview: null,
+      saved: false
+    };
+  }
+
+  render() {
+    return (
+      <p>test</p>
+    );
+  }
+}
