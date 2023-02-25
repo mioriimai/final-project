5acaf62631d402f5f9057f415e50832d6f29df5f
Miori Imai <imamio1227@gmail.com>
2023-02-25 03:24:13 +0000

    Fill the render() method with the box for outfit. #8

--- client/components/form-outfit.jsx
+++ client/components/form-outfit.jsx
@@ -5,4 +5,27 @@ export default class FormOutfit extends React.Component {
     super(props);
     this.state = {};
   }
+
+  render() {
+    return (
+      <form>
+        <div className='form-outfit-container'>
+          <div className='form-outfit-white-box'>
+            <div className='row'>
+              <div className='column-full'>
+                <p className='form-outfit-title'>New Outfit</p>
+              </div>
+            </div>
+            <div className='row'>
+              <div className='column-half'>
+                <div className='outfit-box'>
+                  <p>test</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </form>
+    );
+  }
 }
