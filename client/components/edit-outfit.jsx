d3f54bfffb2ad1a9e36c01bbb870d94db570b73c
Miori Imai <imamio1227@gmail.com>
2023-03-01 00:33:32 +0000

    Add componentDidMount() method and use fetch to GET a specific outfit data with its outfitId. #10

--- client/components/edit-outfit.jsx
+++ client/components/edit-outfit.jsx
@@ -4,10 +4,24 @@ export default class EditOutfit extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      items: [],
+      itemsForOutfit: []
     };
   }
 
+  componentDidMount() {
+    fetch(`/api/outfitItems/${this.props.outfitId}`)
+      .then(res => res.json())
+      .then(itemsForOutfit => this.setState({ itemsForOutfit }));
+
+    fetch('/api/items')
+      .then(res => res.json())
+      .then(items => this.setState({ items }));
+  }
+
   render() {
+
+    // console.log('this.state:', this.state);
     return (
       <p>test</p>
     );
