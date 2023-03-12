c60f838e13aca5748cf26196ccabed8d20a54d7d
Miori Imai <imamio1227@gmail.com>
2023-03-12 23:38:49 +0000

    Update schema.sql and data.sql. #26

--- database/data.sql
+++ database/data.sql
@@ -1,8 +1,8 @@
 -- DANGER: this is NOT how to do it in the real world.
 -- You should NEVER EVER check user data into Git!
 
-insert into "users" ("username", "email", "hashedPassword")
-values ('anonymous', 'abc@gmail.com', '$argon2i$v=19$m=4096,t=3,p=1$h7icQD/xZr8akZsX+hNA0A$h68atJWyjvunAwNOpSpMfg9sPvoMQ6dKwoh0dJhurWA');
+insert into "users" ("username", "name", "hashedPassword")
+values ('anonymous', 'test', '$argon2i$v=19$m=4096,t=3,p=1$h7icQD/xZr8akZsX+hNA0A$h68atJWyjvunAwNOpSpMfg9sPvoMQ6dKwoh0dJhurWA');
 
 insert into "items" ("image", "category", "brand", "color", "favorite", "userId")
 values ('/images/IMG_5668-removebg-preview.png', 'None', 'None', 'None', false, '1'),
