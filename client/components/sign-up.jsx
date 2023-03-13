0f9d42cbbb684be5c40afd6f53e8e85c6da8b6ba
Miori Imai <imamio1227@gmail.com>
2023-03-13 00:04:22 +0000

    Update renderPage() method in app.jsx. #26

---
+++ client/components/sign-up.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default class SignUp extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+      <p>test</p>
+    );
+  }
+}
