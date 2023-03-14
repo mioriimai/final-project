822db80f14eebf1864726721aa75b80762954925
Miori Imai <imamio1227@gmail.com>
2023-03-14 04:45:14 +0000

    Add handleChangeInput() method. #26

--- client/components/sign-up.jsx
+++ client/components/sign-up.jsx
@@ -5,12 +5,30 @@ export default class SignUp extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      name: '',
+      username: '',
+      password: ''
     };
+
+    this.handleChangeInput = this.handleChangeInput.bind(this);
+    // this.handleSubmit = this.handleSubmit.bind(this);
   }
 
+  handleChangeInput(event) {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  }
+
+  // handleSubmit() {
+
+  //   console.log('submitted!');
+  // }
+
   render() {
+    // console.log(this.state);
+
     return (
-      <form>
+      <form /* onSubmit={this.handleSubmit} */>
         <div className='sign-up-container'>
           <div className='sign-up-white-box'>
             <div className='row'>
@@ -19,19 +37,19 @@ export default class SignUp extends React.Component {
             <div className='row'>
               <label htmlFor="name" className='sign-up-name'>
                 Name<br />
-                <input required type="text" id='name'className='sign-up-name-input'/>
+                <input required type="text" id='name' name='name' className='sign-up-name-input' onChange={this.handleChangeInput}/>
               </label>
             </div>
             <div className='row'>
               <label htmlFor="usename" className='sign-up-username'>
                 Username<br />
-                <input required type="text" id='username' className='sign-up-username-input' />
+                <input required type="text" id='username' name='username' className='sign-up-username-input' onChange={this.handleChangeInput} />
               </label>
             </div>
             <div className='row'>
               <label htmlFor="password" className='sign-up-password'>
                 Password<br />
-                <input required type="password" id='passward' className='sign-up-password-input' />
+                <input required type="password" id='passward' name='password' className='sign-up-password-input' onChange={this.handleChangeInput} />
               </label>
             </div>
             <div className='row'>
