5cb3557bcc395c61fe948f075176bcea8b4b8ae9
Miori Imai <imamio1227@gmail.com>
2023-03-16 02:16:37 +0000

    Create a new component called sign-in. #28

---
+++ client/components/sign-in.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default class SignIn extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+      <p>test</p>
+    );
+  }
+}
