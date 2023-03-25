e155ef18d1d7c0f747c916b9a422c5ff65892137
Miori Imai <imamio1227@gmail.com>
2023-03-25 06:05:22 +0000

    Add sign-out button and make sure it looks good on both mobile and desktop.

--- client/components/navbar.jsx
+++ client/components/navbar.jsx
@@ -4,12 +4,18 @@ import AppDrawer from '../components/app-drawer';
 
 export default class Navbar extends React.Component {
   render() {
+    let signInIcon;
+    if (this.context.user) {
+      signInIcon = <><i className="fa-solid fa-right-from-bracket" /><p className='sign-out'>Sign Out</p></>;
+    } else if (!this.context.user) {
+      signInIcon = <i className='fa-solid fa-circle-user' />;
+    }
     return (
       <nav className='navbar-wrapper'>
         <a className='wearly' href='#home'>Wearly</a>
         <AppDrawer />
-        <a href="#sign-up">
-          <i className='fa-solid fa-circle-user'/>
+        <a className="sign-in-icon-wrapper" href="#sign-up">
+          {signInIcon}
         </a>
       </nav>
     );
